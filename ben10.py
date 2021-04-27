c2feff8a881b8f4494e8398bf7fe629293888ee0
superdimensional <plusethunder@gmail.com>
2021-04-27 03:17:12 -0400

    created

---
+++ ben10.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Omnitrix")
+
+count = 0
+
+def count_up():
+    global count
+    count += 1
+    print(count)
+    
+root.geometry("640x480")
+root.configure(bg="#70b607")
+
+omnitrix = PhotoImage(file="omnitrix.png")
+
+omnitrix_button = Button(root, image=omnitrix, bg="#70b607",
+                         activebackground='#70b607', highlightthickness=0, bd=0, command=count_up)
+
+omnitrix_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+root.mainloop()
