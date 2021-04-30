8cdd98ab5333f11bf03acd3c644f27591d2df209
superdimensional <plusethunder@gmail.com>
2021-04-30 01:54:08 -0400

    formated code/added comments

--- aliens.py
+++ aliens.py
@@ -15,27 +15,5 @@ def alien_table(index):
         10: "Ghostfreak",
         11: "Wildvine",
         12: "Heatblast",
-        #TODO: Add more aliens
+        # TODO: Add more aliens
     }.get(index, "ERROR")
-
-
-# screen_heatblast = PhotoImage(file="res/omnitrix.png")
-
-# def image_display(index):
-#     return {
-
-#         0: "res/omnitrix.png",
-#         1: "res/heatblast.png",
-#         2: "Ripjaws",
-#         3: "Diamondhead",
-#         4: "Stinkfly",
-#         5: "Fourarms",
-#         6: "Upgrade",
-#         7: "XLR8",
-#         8: "Grey Matter",
-#         9: "Wildmutt",
-#         10: "Ghostfreak",
-#         11: "Wildvine",
-#         12: "Heatblast",
-
-#     }.get(index, "res/omnitrix.png")
