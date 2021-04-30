f1377920e6fa105920e14699dfec39349269b0d1
superdimensional <plusethunder@gmail.com>
2021-04-30 18:38:49 -0400

    removed deprecated code and formated

--- ben10.py
+++ ben10.py
@@ -15,54 +15,45 @@ root.rowconfigure(1, weight=1)
 
 path = "res/omnitrix.png"
 img = ImageTk.PhotoImage(Image.open(path))
-
-omnitrix_button = Button(root, bg="#70b607", image=img,
-                         activebackground='#70b607', highlightthickness=0, bd=0, command=button_press)
+omnitrix_button = Button(root, bg="#70b607", image=img, activebackground='#70b607',
+                         highlightthickness=0, bd=0, command=button_press)
 # TODO: make sound when pressed
-
 omnitrix_button.image = img
-# omnitrix_button.place(relx=0.5, rely=0.5, anchor=CENTER)
 omnitrix_button.grid(column=1, row=1, rowspan=2, columnspan=2)
 # middle button
 
 
 omnitrix_left_skip = Button(root, text="<<<", fg="white", bg="black",
                             highlightthickness=0, bd=0, height=5, width=4)
-# omnitrix_left_skip.pack(side="left")
 omnitrix_left_skip.grid(column=0, row=0)
 # bulk skip left button
 
 omnitrix_right_skip = Button(root, text=">>>", fg="white",  bg="black",
                              highlightthickness=0, bd=0, height=5, width=4)
-# omnitrix_right_skip.pack(side="right")
 omnitrix_right_skip.grid(column=3, row=0)
 # bulk skip right button
 
 
 omnitrix_left = Button(root, text="<", fg="white", bg="black",
                        command=lambda: count_down(omnitrix_button), highlightthickness=0, bd=0, height=30, width=4)
-# omnitrix_left.pack(side="left")
 omnitrix_left.grid(column=0, row=1, rowspan=3)
-
-
 # changes alien button (left)
 
 omnitrix_right = Button(root, text=">", fg="white",  bg="black",
                         command=lambda: count_up(omnitrix_button), highlightthickness=0, bd=0, height=30, width=4)
-# omnitrix_right.pack(side="right")
 omnitrix_right.grid(column=3, row=1, rowspan=3)
 # changes alien button (right)
 
 
 quit_button = Button(root, text="X", fg="white", bg="black",
-                     command=root.destroy, highlightthickness=0, bd=0, width=5, height=4)
-# quit_button.pack(side="bottom")
+                     command=root.destroy, highlightthickness=0, bd=0, width=10, height=2)
 quit_button.grid(column=1, row=3, columnspan=2)
 # kills program
 
 change_entry = Entry(root, fg="black", bg="white",
                      highlightthickness=0, bd=0, width=5)
-# quit_button.pack(side="bottom")
 change_entry.grid(column=1, row=0, columnspan=2)
-#
+# change to specific entry
+
+
 root.mainloop()
