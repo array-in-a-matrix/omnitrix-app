89db59bebeef69129a6a813f55349e1911de7374
superdimensional <plusethunder@gmail.com>
2021-05-05 03:38:13 -0400

    pip setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+ 
+setup(
+    name='omnitrix-app',
+# This is the name of your PyPI-package.
+    version='0.1',
+# Update the version number for new releases
+    scripts=['omnitrix']
+# The name of your scipt, and also the command you'll be using for calling it
+)
