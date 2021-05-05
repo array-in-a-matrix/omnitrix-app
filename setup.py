fd21d5765b5247a1e93a37fabc0f195bc698d53e
superdimensional <plusethunder@gmail.com>
2021-05-05 04:23:28 -0400

    setup file stuff

--- setup.py
+++ setup.py
@@ -2,9 +2,26 @@ from setuptools import setup
  
 setup(
     name='omnitrix-app',
-# This is the name of your PyPI-package.
-    version='0.1',
-# Update the version number for new releases
-    scripts=['omnitrix']
-# The name of your scipt, and also the command you'll be using for calling it
+    version='1.0',
+    scripts=['omnitrix'],
+    license="gpl-3.0",
+    discription="App that simulates the omnitrix from the ben 10 tv show",
+    author="array-in-a-matrix",
+    url="https://array-in-a-matrix.github.io/",
+    download_url="https://github.com/array-in-a-matrix/raspberry-pi-omnitrix/archive/refs/tags/1.0.tar.gz",
+    keywords = ["game"],
+
+    install_requires=[
+        "tkinter",
+        "PIL",
+        "boombox"
+    ],
+    
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: cosplay',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3.0',   # Again, pick a license
+    'Programming Language :: Python :: 3',
+  ],
 )
