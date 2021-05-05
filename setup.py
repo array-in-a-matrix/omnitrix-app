f658670b82e786740b8ce5168098394e63ed6f71
superdimensional <plusethunder@gmail.com>
2021-05-05 03:47:01 -0400

    idk

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
  
 setup(
-    name='omnitrix',
+    name='omnitrix-app',
 # This is the name of your PyPI-package.
     version='0.1',
 # Update the version number for new releases
